9daee16edf8856f033773a64d0146e3525fce0db
j2FunOnly <j2funonly@ya.ru>
2018-06-26 16:04:21 +0300

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,11 @@ class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :subtitle_items, -> (v) { where(subtitle: v) }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= 'https://placehold.it/600x400'
+    self.thumb_image ||= 'https://placehold.it/350x200'
+  end
 end
