9babf7176ec1aaa0739f21696a9dfdbb812c4843
j2FunOnly <j2funonly@ya.ru>
2018-06-27 12:23:46 +0300

    Integrated nested attributes technologies for portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,9 @@
 class Portfolio < ApplicationRecord
   include Placeholder
 
-  has_many :technologies
+  has_many :technologies, inverse_of: :portfolio
+  accepts_nested_attributes_for :technologies,
+    reject_if: -> (attrs) { attrs['name'].blank? }
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
