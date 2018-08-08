0beac7b55ab3dc238ad35392582ff26fddc3fbfe
j2FunOnly <j2funonly@ya.ru>
2018-08-08 12:14:58 +0300

    Fix portfolio items images placeholder, update file upload fields

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,11 @@
 module PortfoliosHelper
+  def image_generator(width, height)
+    "https://placehold.it/#{width}x#{height}"
+  end
+
+  def portfolio_img(img, type = nil)
+    return img if img.present?
+
+    type == :thumb ? image_generator(350, 200) : image_generator(600, 400)
+  end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,4 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
@@ -8,16 +6,9 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
     reject_if: -> (attrs) { attrs['name'].blank? }
 
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   scope :subtitle_items, -> (v) { where(subtitle: v) }
 
   scope :by_position, -> { order(:position) }
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(600, 400)
-    self.thumb_image ||= Placeholder.image_generator(350, 200)
-  end
 end
